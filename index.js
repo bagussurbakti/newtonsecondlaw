747b304fc9a769ef60c90e11767a8d99f31017e1
dwibagus0512 <dwibagus1998@gmail.com>
2022-04-10 15:51:48 +0700

    Second Law Newton

---
+++ index.js
@@ -0,0 +1,27 @@
+// Insert your code here
+var massa;
+var percepatan;
+
+/**
+ * Algoritma (Hukum Newton ke 2)
+ *
+ * 1. Baca Hukum Algoritma ke 2
+ * 2. Ambil data yang tersedia di soal
+ * 3. Membuat Variabel masa_benda
+ * 4. Membuat Variabel percepatan_benda
+ * 5. Input kedalam Rumus Hukum Newton ke 2
+ * 6. Output dari
+ */
+/**
+ * Pseudocode (Hukum Newton ke 2)
+ * STORE masa_benda WITH ANY NUMBER value 600
+ * STORE percepatan_benda WITH ANY NUMBER value 2
+ * STORE hukumnewton2 AS WITH 0
+ *
+ * CALCULATE masa_benda TIMES percepatan_benda
+ * SET hukumnewton2 WITH VALEU WITH CALCULATE RESULT
+ *
+ * DISPLAY hukumnewton2
+ *
+ *
+ */
